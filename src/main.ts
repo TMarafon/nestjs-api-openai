806256b0ea910433d3ea728653d38e3f0000d4b0
Thiago Antonio Marafon <tmarafon@gmail.com>
2024-05-24 21:59:05 -0300

    Listening to port defined on env

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // the application must listen to PORT env variable or 3000
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
